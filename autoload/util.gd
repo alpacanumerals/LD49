5e042fc56d741ae0bf60f809c7b28c3b1873f5b4
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2021-09-19 22:15:00 -0400

    loading code

--- autoload/util.gd
+++ autoload/util.gd
@@ -1,14 +1,47 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var file_dict
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    randomize()
+    loadJSONs()
     pass # Replace with function body.
 
+func loadJSONs():
+    var file = File.new()
+    file.open("res://loader/loader.txt", File.READ)
+    var loadlist = JSON.parse(file.get_as_text()).result
+    file.close()
+    var loadlist_size = loadlist.size()
+    for i in range(loadlist_size):
+        var file_name = filePath(loadlist[i])
+        file = File.new()
+        file.open((file_name), File.READ)
+        #file_dict dictionary gets file's name as a key and its array appended to it.
+        file_dict[loadlist[i]] = JSON.parse(file.get_as_text()).result
+
+func filePath(file):
+    return "res://loader/"+file+".txt"
+    
+    
 func randomFrom(array):
     return array[randi() % array.size()]
+
+func percentChance(percent):
+    if randi()%100+1 <= percent:
+        return true
+    return false
+
+
+func d100(dice:int = 1):
+    var result:int = 0
+    for i in range(dice):
+        result = result + randi()%100+1 
+    return result
+    
+func d6(dice:int = 1):
+    var result:int = 0
+    for i in range(dice):
+        result = result + randi()%6+1
+    return result

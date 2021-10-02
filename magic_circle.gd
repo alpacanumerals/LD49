6433ea05a1970f2000cfd90fb39b5224db9de9cb
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2021-10-02 19:26:27 +0100

    multiple magic circle slots

--- magic_circle.gd
+++ magic_circle.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 
 # Declare member variables here. Examples:
@@ -9,10 +9,6 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
-
-func get_snap_location():
-    print("honk")
-    return Vector2(500,200)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):

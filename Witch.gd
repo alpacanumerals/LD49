165a13fcfe046ee824249656b323264f6220e257
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2021-10-02 10:46:05 +0100

    click and drag button

--- Witch.gd
+++ Witch.gd
@@ -1,16 +1,22 @@
-extends Area2D
+extends Node2D
 
+var dragging = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal dragsignal;
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	connect("dragsignal", self, "_set_drag_pc")
 
+func _process(delta):
+	if dragging:
+		var mousepos = get_viewport().get_mouse_position()
+		self.global_position = Vector2(mousepos.x, mousepos.y)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func _set_drag_pc():
+	dragging=!dragging
+
+func _on_Button_button_down():
+    emit_signal("dragsignal")
+
+func _on_Button_button_up():
+    emit_signal("dragsignal")

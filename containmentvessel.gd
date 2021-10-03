92f0d2f85cd1814fa6ca32c2211e8b154a15cf5a
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2021-10-03 06:40:38 -0400

    Update containmentvessel.gd

--- containmentvessel.gd
+++ containmentvessel.gd
@@ -24,43 +24,56 @@ func _process(delta):
         velocity.y += 16
     if going_up:
         velocity.y -= 8     
-    update() #This is needed for the cable to draw properly.  
-    position += velocity * delta
     position.x = clamp(position.x, 16, 104)
     position.y = clamp(position.y, 29, 684)
     magic.set_depth(global_position.y)
-                
+    if wobtime == 6.0:
+        velocity.x += 2
+    else:
+        pass
+    wobtime -= delta
+    position += velocity * delta    
+    if position.x > 60:
+        velocity.x -= 0.5
+        breakpoint
+    else:
+        velocity.x += 0.5
     ###TEMPORARY TESTING CODE
 
-#    if Input.is_action_pressed("pmove_right"):
-#        velocity.x += 1
-#    if Input.is_action_pressed("pmove_left"):
-#        velocity.x -= 1
-#    if Input.is_action_pressed("pmove_down"):
-#        velocity.y += 1
-#    if Input.is_action_pressed("pmove_up"):
-#        velocity.y -= 1    
-#    if velocity.length() > 0:
-#        velocity = velocity.normalized() * speed
-#    
+    if Input.is_action_pressed("pmove_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("pmove_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("pmove_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("pmove_up"):
+        velocity.y -= 1    
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+        
+    update() #This is needed for the cable to draw properly.  
 
 func _on_GoingDown_toggled(button_pressed):
     if button_pressed:
         $CableSounds.play()
         going_down = true
-        print(String(going_down))
-        print(String(going_up))
-        
+        wobble = light
+        wobtime = 6.0
     else:   
         $CableSounds.stop()
         $MetalMoan.play()
         going_down = false
-
+        wobble = light
+        wobtime = 6.0
 func _on_GoingUp_toggled(button_pressed):
     if button_pressed:
         $CableSounds.play()
         going_up = true
+        wobble = light
+        wobtime = 6.0
     else:
         $CableSounds.stop()
         $MetalMoan.play()
         going_up = false
+        wobble = light
+        wobtime = 6.0
